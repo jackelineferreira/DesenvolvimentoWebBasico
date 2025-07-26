7eb2021919fd9fd4d92c9ea96b42226549ae681a
Jackeline Ferreira <conecta.online9891@gmail.com>
2025-07-26 02:09:36 -0300

    Primeiro commit: adicionando projeto do DesenvolvimentoWebBasico

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+  const filePath = req.url === '/' ? '/index.html' : req.url;
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+  }
+
+  fs.readFile('./public' + filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Arquivo não encontrado');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+}).listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
